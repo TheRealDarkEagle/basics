fc52d4126b16814417a9aba70a38839466e38052
danzk <danzk@WNB30306.bboffice.baur.net>
2018-10-31 15:13:59 +0100

    viele testings rausgelöscht weil commit erklärt wird

--- basics/src/com/empirie/basics/taschenrechner/Tester.java
+++ basics/src/com/empirie/basics/taschenrechner/Tester.java
@@ -8,16 +8,6 @@ public class Tester {
 		to.isValid("5(6+3-5)");
 		to.isValid("6(5/6-5*5)*(2+3-4)");
 		to.isValid("9(2+3)*(4+6)");
-		to.isValid("46*5(4+69-741/2.5)");
-		to.isValid("-1+(8*9-3/5(4+(5)-3/8)-5)/-2");
-		to.isValid("1-2");
-        to.isValid("8/(-2(3))");    
-        to.isValid(" 2*-25.5");    
-        to.isValid("-2-(-6)");    
-        to.isValid("6-5+8.333332-15");    
-        to.isValid("2+25");    
-        to.isValid("1(1*(2+3))");
-        to.isValid("-2+((-6-3)-2)");    //-> hier endlos schleife
 		to.isValid("(8 + 2) / 5  ");   // -> hier fehler
 		to.isValid("2*3-5 + ((8 - (3 + 2) / 3 ) * 1 + 2 / 1) -15"); // hier fehler    
        // to.isValid("2*(-2+((-6-3)-2)/2-3+(-5-2*5))");   // -> endlos schleife
