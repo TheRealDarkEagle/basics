131bfe25eb1997bf9d9f0f1ff2e88c2ed3bac9dd
danzk <danzk@WNB30306.bboffice.baur.net>
2018-11-06 12:46:25 +0100

    Überarbeitung der Methoden:
    isWin:
    nimmt nun die eingegebenen Buchstaben, setzt diese in einem String
    zusammen und gleicht diesen mit dem Startwort ab
    
    hangingManVisual:
    Visuelle Ausgabe eines "hängenden Mannes"
    
    NEW getWordFromList:
    Liste mit 10 verschiendenen Spielwörtern
    wählt beim aufrufen eines der Wörter, anhand veiner zufällig generierten
    Zahl, aus.

---
+++ basics/src/work_from_home/Hangman.java
@@ -0,0 +1,315 @@
+package work_from_home;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class Hangman {
+
+	/**
+	 * @ToDo	visuellen hangman machen 
+	 * 			
+	 * 
+	 */
+	
+	static int life =8;
+	int toWin;
+	int isRight;
+	private static Scanner character;
+	char[] usedChars = new char[25];
+	int u = 0;
+	
+	/**
+	 * Startpunkt der Klasse -> aufrufen aller anderen Methoden 
+	 */
+	public void startGame() {
+		String wort = getWordFromList();
+		wort = toSmall(wort);
+		toWin = wort.length();
+		char[] theWord = new char[wort.length()];
+		for(int i=0;i<theWord.length;i++) {
+			theWord[i]='.';
+		}
+		for(int s=0;s<usedChars.length;s++) {
+			usedChars[s]='.';
+		}
+		gameLogic(wort,theWord);
+		
+	}
+
+	/**
+	 * Eingabe des zu erratenden Wortes -> momentan Obsolet!
+	 * @return
+	 */
+//	private String insertWord() {
+//		System.out.println("Geben Sie bitte Ihr Wort ein!: ");
+//		Scanner sc = new Scanner(System.in);
+//		String wort = sc.next();
+//		sc.close();
+//		wort = toSmall(wort);
+//		return wort;
+//	}
+	
+	/**
+	 * Eingabe des Buchstaben 
+	 * @return
+	 */
+	private static char letter() {
+		character = new Scanner(System.in);
+		char c = character.next().charAt(0);
+		return c;
+	}
+	
+	/**
+	 * Kleinschreibung des zu suchenden Wortes 
+	 * @param wort
+	 * @return
+	 */
+	
+	private static String toSmall(String wort) {
+		wort = wort.toLowerCase();
+		return wort;
+	}
+	
+	/**
+	 * befüllung der leeren Array Felder mit . -> wenn in array[i]=='.' darstellung durch _ 
+	 * @param theWord
+	 */
+	private static void printOnScreen(char[] theWord) {
+		for(int i=0;i<theWord.length;i++) {
+			if(theWord[i]!='.') {
+				System.out.print(theWord[i]+" ");
+			}else {
+				System.out.print("_ ");
+				
+			}
+		}
+	}
+
+	/**
+	 * Prüft bisher eingegebene Buchstaben ob zusammengesetzt gleich dem Suchwort.
+	 * @param wort
+	 * @param theWord
+	 * @return
+	 */
+	private String isWin(String wort,char[] theWord) {
+		String guessedWord ="";
+		for(int i=0;i<theWord.length;i++) {
+			guessedWord = guessedWord+theWord[i];		
+		}
+		if(wort.equals(guessedWord)) {
+			printOnScreen(theWord);
+			System.out.println();
+			System.out.println("Gewonnen!");
+			return "Hurra!! Gewonnen!";
+		}else {
+			return gameLogic(wort,theWord);
+			
+		}
+	}
+	
+	/**
+	 * Aufruf aller Spielmethoden solange leben!=0
+	 * @param wort
+	 * @param theWord
+	 * @return
+	 */
+	private String gameLogic(String wort,char[] theWord) {
+		while(life != 0) {
+			hangingManVisual(life);
+			System.out.println("Sie haben noch "+life+" Versuche");
+			printOnScreen(theWord);
+			System.out.println();
+			System.out.println("Bitte geben sie Ihren Buchstaben ein:");
+			Character toTest = letter();
+			String a = toTest.toString();
+			//prüfe ob buchstabe vorhanden
+			if(wort.contains(a)) {
+				for(int i=0;i<wort.length();i++) {
+					if(wort.charAt(i)==toTest) {
+						theWord[i]=toTest;
+						isRight+=1;
+					}
+				}
+				alreadyUsedChars(toTest);
+				return isWin(wort,theWord);
+			}else {
+				life -=1;
+				alreadyUsedChars(toTest);
+				return gameLogic(wort,theWord);
+			}
+		}
+		hangingManVisual(life);
+		System.out.println("Leider Verloren :(");
+		return wort;
+	}
+	
+	/**
+	 * Array welches bisher eingegebene Buchstaben beeinhaltet - Prüft ob Buchstabe schon eingegeben wurde
+	 * @param used
+	 */
+	private void alreadyUsedChars(char used) {
+		for(int i=0;i<usedChars.length;i++) {
+			if(usedChars[i]==used) {
+				System.out.println();
+				System.out.println("Fehler! Buchstabe schon benutzt! Bitte versuchen Sie einen anderen Buchstaben");
+				System.out.println();
+				return;
+			}
+		}
+		usedChars[u] = used;
+		System.out.println();
+		System.out.println("Ihre benutzten Buchstaben:");
+		for(int o=0;o<usedChars.length;o++) {
+			if(usedChars[o]!='.') {
+				System.out.print(usedChars[o]+" ");	
+			}
+		}
+		System.out.println();
+		System.out.println();
+		u++;
+	}
+	
+	/**
+	 * Liste mit möglichen Wörtern und heraussuchen eines per Zufall
+	 * @return
+	 */
+	//Erstellen einer Liste mit Wörter
+private String getWordFromList() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Stromaggregat");
+		list.add("Computergehaeuse");
+		list.add("Feuerwerk");
+		list.add("Feuerwehr");
+		list.add("Puzzleteil");
+		list.add("Pizzateig");
+		list.add("Gleichberechtigungsbeauftragter");
+		list.add("Haengewandschrankhalterung");
+		list.add("lokomotive");
+		list.add("photovoltaikanlage");
+		int i = random(list.size());
+		return list.get(i);
+	}
+	
+	/**
+	 * Erzeugen einer zufälligen Zahl zwischen 0 bis 10
+	 * @param maxNumber
+	 * @return
+	 */
+	private int random(int maxNumber) {
+		Random wuerfel = new Random();
+		int zahl = 0+wuerfel.nextInt(maxNumber);
+		return zahl;
+	}
+
+	/**
+	 * Erzeugen des Visuellen "Hangman" Fortschritt anhand von restlichen Leben
+	 * @param life
+	 */
+	private void hangingManVisual(int life) {
+		switch(life) {
+		case 8:
+			System.out.println(" ________");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 7:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 6:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |      0");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 5:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |      0");
+			System.out.println(" |      |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 4:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |      0");
+			System.out.println(" |     /|");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 3:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |      0");
+			System.out.println(" |     /|");
+			System.out.println(" |      |");
+			System.out.println(" |");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 2:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |      0");
+			System.out.println(" |     /|");
+			System.out.println(" |      |");
+			System.out.println(" |     /");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 1:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |      0");
+			System.out.println(" |     /|");
+			System.out.println(" |      |");
+			System.out.println(" |     / \\");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		case 0:
+			System.out.println(" ________");
+			System.out.println(" |      |");
+			System.out.println(" |      X");
+			System.out.println(" |     /|\\");
+			System.out.println(" |      |");
+			System.out.println(" |     / \\");
+			System.out.println("_|____");
+			System.out.println("|     |___");
+			System.out.println("|_________|");
+			break;
+		}
+	}
+
+}
+	
---
+++ basics/src/work_from_home/HangmanMain.java
@@ -0,0 +1,9 @@
+package work_from_home;
+
+public class HangmanMain {
+	
+	public static void main (String[] args) {
+		Hangman man = new Hangman();
+		man.startGame();
+	}
+}
