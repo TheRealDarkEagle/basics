15cb3b69e050ab2b3b195c80ef2bd5af19a23c59
danzk <danzk@WNB30306.bboffice.baur.net>
2018-11-21 09:22:23 +0100

    Form-Verbesserung
    Überarbeitung der Globane Variablen
    Überarbeitung der Visuellen Ausgabe
    Umwandlung von char[] used Chars in list

--- basics/src/work_from_home/Hangman.java
+++ basics/src/work_from_home/Hangman.java
@@ -6,7 +6,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 
 public class Hangman {
@@ -20,18 +19,11 @@ public class Hangman {
 	 */
 	
 	//überarbeiten
-	private boolean isFirstCharUpperCase = false;
-	private int life =8;
-	private Scanner character;
-	private char[] usedChars = new char[25];
-	private int u = 0;
 	// wo brauchen wir computer wirklich? Macht es als Globale Variable Sinn?
 	private ResolveHangman computer;
-	private ArrayList<String> list = new ArrayList<String>();
 	private boolean isHumanPlaying = false;
-	private static boolean wordBeginsUpperCase;
 	
-	public Hangman(boolean isHumanPlaying) {
+	public Hangman(boolean isHumanPlaying) throws IOException {
 		this.isHumanPlaying = isHumanPlaying;
 	}
 	
@@ -41,45 +33,36 @@ public class Hangman {
 	 * @throws IOException 
 	 */
 	public void startGame() throws IOException {
+		int win = 0;
+		ArrayList<Character> alreadyUsedCharList = new ArrayList<Character>();
 		if(!isHumanPlaying) {
 			this.computer = new ResolveHangman();
 		}
+		int life =8;
+		System.out.println("Bitte geben Sie Ihr Wort ein.");
 		String wort = insertWordManuel();
-//		wort = changeWierdChars(wort);
-		isFirstCharUpperCase(wort);
+		boolean wordBeginsUpperCase = isFirstCharUpperCase(wort);
 		wort = toSmall(wort);
 		char[] theWord = new char[wort.length()];
 		for(int i=0;i<theWord.length;i++) {
 			theWord[i]='.';
 		}
-		for(int s=0;s<usedChars.length;s++) {
-			usedChars[s]='.';
-		}
-		gameLogic(wort,theWord);
+		gameLogic(wort,theWord,life,alreadyUsedCharList, win, wordBeginsUpperCase);
 	}
 	
 	/**
-	 * Pr�ft ob erster Buchstabe gro� oder kleingeschrieben ist
+	 * Prüft ob erster Buchstabe groß oder kleingeschrieben ist
 	 * @param wort
 	 */
-	private void isFirstCharUpperCase(String wort) {
-		String posibilUpperChars ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	private boolean isFirstCharUpperCase(String wort) {
+		String posibilUpperChars ="ABCDEFGHIJKLMNOPQRSTUVWXYZÜÄÖ";
 		Character firstChar = wort.charAt(0);
 		for(int i =0;i<posibilUpperChars.length();i++) {
 			if(firstChar.equals(posibilUpperChars.charAt(i))) {
-				wordBeginsUpperCase = true;
+				return true;
 			}
 		}
-	}
-	
-	/**
-	 * Eingabe des Buchstaben 
-	 * @return
-	 */
-	public char letter() {
-		character = new Scanner(System.in);
-		char c = character.next().charAt(0);
-		return c;
+		return false;
 	}
 	
 	/**
@@ -93,11 +76,11 @@ public class Hangman {
 	}
 	
 	/**
-	 * bef�llung der leeren Array Felder mit . -> wenn in array[i]=='.' darstellung durch _ 
+	 * befüllung der leeren Array Felder mit . -> wenn in array[i]=='.' darstellung durch _ 
 	 * @param theWord
 	 */
 	//ausgabe überarbeiten 
-	private static void printOnScreen(char[] theWord) {
+	private void printOnScreen(char[] theWord, boolean wordBeginsUpperCase) {
 		for(int i=0;i<theWord.length;i++) {
 			if(theWord[i]!='.') {
 				if(i==0) {
@@ -113,29 +96,27 @@ public class Hangman {
 				
 			}
 		}
+		System.out.println();
 	}
 
 	/**
-	 * Pr�ft bisher eingegebene Buchstaben ob zusammengesetzt gleich dem Suchwort.
+	 * Prüft bisher eingegebene Buchstaben ob zusammengesetzt gleich dem Suchwort.
 	 * @param wort
 	 * @param theWord
 	 * @return
 	 * @throws IOException 
 	 */
-	private String isWin(String wort,char[] theWord) throws IOException {
+	private int isWin(String wort,char[] theWord, boolean wordBeginsUpperCase) throws IOException {
 		String guessedWord ="";
 		for(int i=0;i<theWord.length;i++) {
 			guessedWord = guessedWord+theWord[i];		
 		}
 		if(wort.toLowerCase().equals(guessedWord.toLowerCase())) {
-			printOnScreen(theWord);
-			System.out.println();
-			System.out.println("Gewonnen!");
-			visualWin();
-			return "";
+			printOnScreen(theWord, wordBeginsUpperCase);
+			return 1;
 			
 		}else {
-			return gameLogic(wort,theWord);
+			return 0;
 			
 		}
 	}
@@ -147,48 +128,55 @@ public class Hangman {
 	 * @return
 	 * @throws IOException 
 	 */
-	private String gameLogic(String wort,char[] theWord) throws IOException {
-		while(life != 0) {
-			hangingManVisual();
+	private void gameLogic(String wort,char[] theWord, int life, ArrayList<Character> alreadyUsedCharList,int win, boolean wordBeginsUpperCase) throws IOException {
+		while(life >= 1 && win == 0){
 			System.out.println("Sie haben noch "+life+" Versuche");
-			printOnScreen(theWord);
+			printOnScreen(theWord, wordBeginsUpperCase);
 			System.out.println();
-			System.out.println("Bitte geben sie Ihren Buchst<aben ein:");
 			Character toTest = ' ';
 			if(isHumanPlaying) {
-				toTest = letter();
+				System.out.println("Bitte geben Sie Ihren Buchstaben ein.");
+				toTest = insertWordManuel().charAt(0);
 			}else {
-				// wenn Buchstabe = an erster stelle -> "a".toUpperCase();
-				// 
-				
-				
+				System.out.println("Bitte warten... Buchstabe wird berechnet");
 				toTest = computer.getChar(theWord);
 			}
 			
-			String a = toTest.toString();
-			//pr�fe ob buchstabe vorhanden
-			if(wort.contains(a)) {
+			String insertChar = toTest.toString();
+			//prüfe ob buchstabe vorhanden
+			if(wort.contains(insertChar)) {
 				for(int i=0;i<wort.length();i++) {
 					if(wort.charAt(i)==toTest) {
 						theWord[i]=toTest;
 					}
 				}
-				alreadyUsedChars(toTest);
-				return isWin(wort,theWord);
+				alreadyUsedChars(toTest,alreadyUsedCharList);
+				
 			}else {
 				life -=1;
-				alreadyUsedChars(toTest);
-				return gameLogic(wort,theWord);
+				alreadyUsedChars(toTest,alreadyUsedCharList);
+//				gameLogic(wort,theWord, life,alreadyUsedCharList,win);
 			}
+			win = isWin(wort,theWord, wordBeginsUpperCase);
+			visualOut(life, win);
 		}
-		hangingManVisual();
-		System.out.println("Leider Verloren :(");
+		if(life == 0) {
+			System.out.println("Leider Verloren :(");
+			return;
+		}
+		else if(win == 1) {
+			System.out.println("GEWONNEN!");
+			return;
+		}
+		if(!isHumanPlaying) {
 		writeIntoWordGlossar(wort,wordBeginsUpperCase);
-		return wort;
+		}
+		
+		gameLogic(wort, theWord, life, alreadyUsedCharList, win, wordBeginsUpperCase);
 	}
 	
 	/**
-	 * F�gt Wort (Wenn nicht gefunden) in Glossar hinzu
+	 * Fügt Wort in Glossar hinzu
 	 * @param wort
 	 * @param wordBeginsUpperCase
 	 * @throws IOException
@@ -209,154 +197,122 @@ public class Hangman {
 	}
 
 
-	/**
-	 * Array welches bisher eingegebene Buchstaben beeinhaltet - Pr�ft ob Buchstabe schon eingegeben wurde
-	 * @param used
-	 */
-	private void alreadyUsedChars(char used) {
-		for(int i=0;i<usedChars.length;i++) {
-			if(usedChars[i]==used) {
-				System.out.println();
-				System.out.println("Fehler! Buchstabe schon benutzt! Bitte versuchen Sie einen anderen Buchstaben");
-				System.out.println();
-				return;
-			}
+	private void alreadyUsedChars(char used, ArrayList<Character> alreadyUsedCharList) {
+		if(alreadyUsedCharList.contains(used)) {
+			System.out.println();
+			System.out.println("Fehler! Buchstabe schon benutzt! Bitte versuchen Sie einen anderen Buchstaben");
+			System.out.println();
+			return;
 		}
-		usedChars[u] = used;
+		alreadyUsedCharList.add(used);
 		System.out.println();
 		System.out.println("Ihre benutzten Buchstaben:");
-		for(int o=0;o<usedChars.length;o++) {
-			if(usedChars[o]!='.') {
-				System.out.print(usedChars[o]+" ");	
-			}
-		}
+		System.out.println(alreadyUsedCharList);
 		System.out.println();
-		System.out.println();
-		u++;
 	}
 	
-	/**
-	 * Erzeugen des Visuellen "Hangman" Fortschritt anhand von restlichen Leben
-	 * @param life
-	 */
-	private void hangingManVisual() {
-		switch(life) {
-		case 8:
-			System.out.println(" ________");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 7:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 6:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |      0");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 5:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |      0");
-			System.out.println(" |      |");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 4:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |      0");
-			System.out.println(" |     /|");
-			System.out.println(" |");
-			System.out.println(" |");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 3:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |      0");
-			System.out.println(" |     /|");
-			System.out.println(" |      |");
-			System.out.println(" |");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 2:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |      0");
-			System.out.println(" |     /|");
-			System.out.println(" |      |");
-			System.out.println(" |     /");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 1:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |      0");
-			System.out.println(" |     /|");
-			System.out.println(" |      |");
-			System.out.println(" |     / \\");
-			System.out.println("_|____");
-			System.out.println("|     |___");
-			System.out.println("|_________|");
-			break;
-		case 0:
-			System.out.println(" ________");
-			System.out.println(" |      |");
-			System.out.println(" |      X");
-			System.out.println(" |     /|\\");
-			System.out.println(" |      |");
-			System.out.println(" |     / \\");
-			break;
+
+	
+	private void visualOut(int life, int isWin) {
+		System.out.println(" ________");
+		switch(isWin){
+			case 0:
+				switch(life) {
+				case 8:
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				case 7:
+					System.out.println(" |      |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				case 6:
+					System.out.println(" |      |");
+					System.out.println(" |      0");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				case 5:
+					System.out.println(" |      |");
+					System.out.println(" |      0");
+					System.out.println(" |      |");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println("|     |___");
+					break;
+				case 4:
+					System.out.println(" |      |");
+					System.out.println(" |      0");
+					System.out.println(" |     /|");
+					System.out.println(" |");
+					System.out.println(" |");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				case 3:
+					System.out.println(" |      |");
+					System.out.println(" |      0");
+					System.out.println(" |     /|");
+					System.out.println(" |      |");
+					System.out.println(" |");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				case 2:
+					System.out.println(" |      |");
+					System.out.println(" |      0");
+					System.out.println(" |     /|");
+					System.out.println(" |      |");
+					System.out.println(" |     /");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				case 1:
+					System.out.println(" |      |");
+					System.out.println(" |      0");
+					System.out.println(" |     /|");
+					System.out.println(" |      |");
+					System.out.println(" |     / \\");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				case 0:
+					System.out.println(" |      |");
+					System.out.println(" |      X");
+					System.out.println(" |     /|\\");
+					System.out.println(" |      |");
+					System.out.println(" |     / \\");
+					System.out.println("_|____");
+					System.out.println("|     |___");
+					break;
+				}
+				break;
+				
+			case 1:
+				System.out.println(" |      |");
+				System.out.println(" |        ");
+				System.out.println(" |        ");
+				System.out.println(" |      0 ");
+				System.out.println(" |     \\|/  ");
+				System.out.println("_|____  |  ");
+				System.out.println("|     |/_\\_");
+				break;
 		}
-		System.out.println("_|____");
-		System.out.println("|     |___");
 		System.out.println("|_________|");
 	}
-
-	/**
-	 * Visuell Hangman v.Gewonnen
-	 */
-	private void visualWin() {
-		System.out.println(" ________");
-		System.out.println(" |      |");
-		System.out.println(" |        ");
-		System.out.println(" |        ");
-		System.out.println(" |      0 ");
-		System.out.println(" |     \\|/  ");
-		System.out.println("_|____  |  ");
-		System.out.println("|     |/_\\_");
-		System.out.println("|_________|");
-		}
 
 	/**
 	 * Wort wird manuell eingefügt
@@ -366,7 +322,6 @@ public class Hangman {
 	private String insertWordManuel() throws IOException {
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		System.out.println("Bitte geben Sie ihr Wort ein: ");
 		
 		String eingabe = br.readLine();
 		return eingabe;
